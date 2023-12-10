f25593376553ab3218d1330df581fccd4d99226f
yang <yang@example.com>
2023-12-10 15:54:19 +0800

    test

--- 创建型_外观模式.py
+++ 创建型_外观模式.py
@@ -1,1 +1,25 @@
-from abc import 
+from abc import ABCMeta, abstractmethod
+
+
+class CPU:
+    def run(self):
+        print("cpu开始工作")
+
+    def stop(self):
+        print("cpu停止工作")
+
+
+class Disk:
+    def run(self):
+        print("硬盘开始工作")
+
+    def stop(self):
+        print("硬盘停止工作")
+
+
+class Memory:
+    def run(self):
+        print("内存开始工作")
+
+    def stop(self):
+        print("内存停止工作")
